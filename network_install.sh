c9aca7a0ff6d899e3b33b5dddff1478d28ba8e43
Martin Klomp <m.klomp@tudelft.nl>
2019-11-08 20:08:19 +0000

    Fixes for install script

--- network_install.sh
+++ network_install.sh
@@ -6,8 +6,10 @@ systemctl disable systemd-resolved
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 apt install -y dnsmasq
 systemctl disable dnsmasq # will be enabled by wifi-connect
+systemctl disable hostapd
 
 # Added systemd service to account for fix: https://askubuntu.com/questions/472794/hostapd-error-nl80211-could-not-configure-driver-mode
+sudo rm /lib/systemd/system/zoef_ap.service
 echo '[Unit]' >> /lib/systemd/system/zoef_ap.service
 echo 'Description=Zoef Wifi AP' >> /lib/systemd/system/zoef_ap.service
 echo 'After=network.target' >> /lib/systemd/system/zoef_ap.service
@@ -15,7 +17,7 @@ echo 'After=ssh.service' >> /lib/systemd/system/zoef_ap.service
 echo 'After=network-online.target' >> /lib/systemd/system/zoef_ap.service
 echo '' >> /lib/systemd/system/zoef_ap.service
 echo '[Service]' >> /lib/systemd/system/zoef_ap.service
-echo 'ExecStart=/bin/bash -c "/home/zoef/zoef_sd_card_image/network_setup.sh"' >> /lib/systemd/system/zoef_ap.service
+echo 'ExecStart=/bin/bash -c "/home/zoef/zoef_install_scripts/network_setup.sh"' >> /lib/systemd/system/zoef_ap.service
 echo '' >> /lib/systemd/system/zoef_ap.service
 echo '[Install]' >> /lib/systemd/system/zoef_ap.service
 echo 'WantedBy=multi-user.target' >> /lib/systemd/system/zoef_ap.service
