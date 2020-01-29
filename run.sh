39f0344261d8574ba4d3a44960abf84ea59b5306
Martin Klomp <m.klomp@tudelft.nl>
2020-01-29 19:59:32 +0000

    Temp fix for non working singularity app

--- run.sh
+++ run.sh
@@ -3,11 +3,13 @@
 echo 
 if test -z $1  || test "$1" == "upload"
 then
-   sudo singularity run --app upload --bind ino_project:/ino_project arduino_utils
+#   sudo singularity run --app upload --bind ino_project:/ino_project arduino_utils
+    sudo singularity exec --bind ino_project:/ino_project arduino_utils/ /bin/bash -c "cd /ino_project && rm -rf .build && ino build -m nano328 && ino upload -m nano328"
 fi
 if test "$1" == "upload_old"
 then
-   sudo singularity run --app upload_old --bind ino_project:/ino_project arduino_utils
+#   sudo singularity run --app upload_old --bind ino_project:/ino_project arduino_utils
+    sudo singularity exec --bind ino_project:/ino_project arduino_utils/ /bin/bash -c "cd /ino_project && rm -rf .build && ino build -m nano328 && cp -r .build/nano328 .build/mini328 && ino upload -m mini328"
 fi
 if test "$1" == "build"
 then
