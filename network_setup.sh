812512aff632e5a62226e7e5ee174916f3eeae15
Martin Klomp <m.klomp@tudelft.nl>
2020-01-31 11:48:12 +0000

    Blinking SSID or IP on OPi

--- network_setup.sh
+++ network_setup.sh
@@ -4,6 +4,8 @@ if [ ! -f /etc/ssid ]; then
     UNIQUE_ID=$(openssl rand -hex 3)
     ZOEF_SSID=Zoef_$(echo ${UNIQUE_ID^^})
     sudo bash -c 'echo '$ZOEF_SSID' > /etc/ssid'
+else
+    UNIQUE_ID=$(cat /etc/ssid | cut -c6-11)
 fi
 
 if [ ! -f /etc/wifi_pwd ]; then
@@ -17,8 +19,10 @@ iwgetid -r
 
 if [ $? -eq 0 ]; then
     printf 'Skipping WiFi Connect\n'
+    sudo /home/zoef/zoef_install_scripts/blink.sh $(hostname -I)
 else
     printf 'Starting WiFi Connect\n'
+    sudo /home/zoef/zoef_install_scripts/blink.sh $UNIQUE_ID > /home/zoef/blink.log &
     sudo wifi-connect -o 8080 -p `cat /etc/wifi_pwd` -s `cat /etc/ssid`
     sudo service dnsmasq start
 fi
