fc16ffd46cfa3a36f282e1a62309042a17a6258d
Martin Klomp <m.klomp@tudelft.nl>
2020-03-16 16:14:15 +0100

    Added firmata for STM32

--- install.sh
+++ install.sh
@@ -2,29 +2,27 @@
 
 # Install FirmataPlus
 sudo apt install -y wget unzip git
-mkdir ./ino_project
-cd ./ino_project
+rm -rf ./arduino_project
+mkdir -p ./arduino_project/FirmataPlusDue
+cd ./arduino_project/FirmataPlusDue
 wget https://github.com/MrYsLab/pymata-aio/raw/master/FirmataPlus/libraries.zip
 unzip libraries.zip
-mv libraries lib
 rm -rf libraries.zip
-mkdir src
-
-# Replace default FirmataPlus with our own version
-cd lib
+cd libraries
 git clone https://gitlab.tudelft.nl/rcj_zoef/zoef_firmata
+cd zoef_firmata
+git checkout --track origin/firmata-stm32
 cd ..
-cp ./lib/zoef_firmata/examples/FirmataPlus/FirmataPlus.ino ./src
-mv ./lib/zoef_firmata/libraries/* ./lib
-cd lib
-rm -rf FirmataPlus
-mv zoef_firmata FirmataPlus
+mv zoef_firmata FirmataPlusDue
+cd ..
+cp libraries/FirmataPlusDue/examples/FirmataPlusDue/FirmataPlusDue.ino .
+cp libraries/FirmataPlusDue/libraries/* libraries/ -R
 cd ../..
 
 # Install singularity image 
 sudo apt install -y singularity-container
-rm -rf arduino_utils
+sudo rm -rf arduino_utils
 sudo singularity build --sandbox arduino_utils Singularity
 
 # Enable usbmon
-echo usbmon | sudo tee -a /etc/modules
+#echo usbmon | sudo tee -a /etc/modules
--- run.sh
+++ run.sh
@@ -4,21 +4,24 @@ sudo service zoef_ros stop
 echo 
 if test -z $1  || test "$1" == "upload"
 then
-#   sudo singularity run --app upload --bind ino_project:/ino_project arduino_utils
-    sudo singularity exec --bind ino_project:/ino_project arduino_utils/ /bin/bash -c "cd /ino_project && rm -rf .build && ino build -m nano328 && ino upload -m nano328"
+    sudo singularity run --app upload --bind arduino_project:/arduino_project arduino_utils
 fi
 if test "$1" == "upload_old"
 then
-#   sudo singularity run --app upload_old --bind ino_project:/ino_project arduino_utils
-    sudo singularity exec --bind ino_project:/ino_project arduino_utils/ /bin/bash -c "cd /ino_project && rm -rf .build && ino build -m nano328 && cp -r .build/nano328 .build/mini328 && ino upload -m mini328"
+    sudo singularity run --app upload_old --bind arduino_project:/arduino_project arduino_utils
 fi
+if test "$1" == "upload_stm32"
+then
+    sudo singularity run --app upload_stm32 --bind arduino_project:/arduino_project arduino_utils
+fi
+
 if test "$1" == "build"
 then
    sudo singularity run --app build --bind ino_project:/ino_project arduino_utils
 fi
-if test "$1" == "monitor"
-then
-   sudo singularity run --app monitor arduino_utils
-fi
+#if test "$1" == "monitor"
+#then
+#   sudo singularity run --app monitor arduino_utils
+#fi
 
 sudo service zoef_ros start
