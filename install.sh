db74fcd4e85799f66c2994d5f9f5f174120d868e
Martin Klomp <m.klomp@tudelft.nl>
2020-03-25 08:56:13 +0100

    Added to dialout group

--- install.sh
+++ install.sh
@@ -25,5 +25,8 @@ sudo apt install -y singularity-container
 sudo rm -rf arduino_utils
 sudo singularity build --sandbox arduino_utils Singularity
 
+# Add zoef to dialout
+sudo adduser zoef dialout
+
 # Enable usbmon
 #echo usbmon | sudo tee -a /etc/modules
