7816065ee4017c50b8032b612e47a03119df0af9
Martin Klomp <m.klomp@tudelft.nl>
2020-03-26 08:34:06 +0100

    Fix for install script

--- network_install.sh
+++ network_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
+ZOEF_SRC_DIR=/usr/local/src/zoef
+
 # Install wifi-connect
 bash <(curl -L https://github.com/balena-io/wifi-connect/raw/master/scripts/raspbian-install.sh)
 systemctl disable systemd-resolved
@@ -10,13 +12,9 @@ systemctl disable hostapd
 
 # Added systemd service to account for fix: https://askubuntu.com/questions/472794/hostapd-error-nl80211-could-not-configure-driver-mode
 sudo rm /lib/systemd/system/zoef_ap.service
-sudo ln -s ./services/zoef_ap.service /lib/systemd/system/
+sudo ln -s $ZOEF_SRC_DIR/zoef_install_scripts/services/zoef_ap.service /lib/systemd/system/
 
 sudo systemctl daemon-reload
 sudo systemctl stop zoef_ap || /bin/true
 sudo systemctl start zoef_ap
 sudo systemctl enable zoef_ap
-
-# Add avahi daemon to enable http://zoef.local
-sudo apt-get install -y avahi-daemon
-echo "zoef" > /etc/hostname
