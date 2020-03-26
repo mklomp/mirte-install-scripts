9a203b72c5fbe0516156ed168e8b21032ce6db41
Martin Klomp <m.klomp@tudelft.nl>
2020-03-26 20:55:33 +0100

    Fixes for ROS install

--- install_ROS.sh
+++ install_ROS.sh
@@ -7,15 +7,15 @@ ZOEF_SRC_DIR=/usr/local/src/zoef
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
-sudo apt install -y ros-melodic-ros-base python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-tools
-sudo rosdep init
-rosdep update
+sudo apt install -y ros-melodic-ros-base python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-tools
 grep -qxF "source /opt/ros/melodic/setup.bash" /home/zoef/.bashrc || echo "source /opt/ros/melodic/setup.bash" >> /home/zoef/.bashrc
 source /opt/ros/melodic/setup.bash
+sudo rosdep init
+rosdep update
 
 # Install computer vision libraries
 #TODO: make dependecies of ROS package
-sudo apt install python-pip python-opencv libzbar0 -y
+sudo apt install python-pip python-wheel python-setuptools python-opencv libzbar0 -y
 sudo -H python -m pip install pyzbar
 
 # Install Zoef ROS package
