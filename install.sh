9c7cd1ad9de0d5b22da44fc024ca136dbc966b31
Martin Klomp <m.klomp@tudelft.nl>
2020-03-26 08:52:46 +0100

    Moved installation of Firmata project to install scripts

--- install.sh
+++ install.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
 
-# Install FirmataPlus
-sudo apt install -y wget unzip git
-rm -rf ./arduino_project
-mkdir -p ./arduino_project/FirmataPlusDue
-cd ./arduino_project/FirmataPlusDue
-wget https://github.com/MrYsLab/pymata-aio/raw/master/FirmataPlus/libraries.zip
-unzip libraries.zip
-rm -rf libraries.zip
-cd libraries
-git clone https://gitlab.tudelft.nl/rcj_zoef/zoef_firmata
-cd zoef_firmata
-git checkout --track origin/firmata-stm32
-cd ..
-mv zoef_firmata FirmataPlusDue
-cd ..
-ln -s libraries/FirmataPlusDue/examples/FirmataPlusDue/FirmataPlusDue.ino .
-ln -s libraries/FirmataPlusDue/libraries/OpticalEncoder libraries/
-cd ../..
-
 # Install singularity image
-# TODO: change Singularity file to install lib23z1 when on 64 bit machine
 sudo apt install -y singularity-container
 sudo rm -rf arduino_utils
 sudo singularity build --sandbox arduino_utils Singularity
--- run.sh
+++ run.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-sudo service zoef_ros stop
+sudo service zoef_ros stop || /bin/true
+
 echo 
 if test -z $1  || test "$1" == "upload"
 then
-    sudo singularity run --app upload --bind arduino_project:/arduino_project arduino_utils
+    sudo singularity run --app upload --bind /home/zoef/arduino_project:/arduino_project arduino_utils
 fi
 if test "$1" == "upload_old"
 then
-    sudo singularity run --app upload_old --bind arduino_project:/arduino_project arduino_utils
+    sudo singularity run --app upload_old --bind /home/zoef/arduino_project:/arduino_project arduino_utils
 fi
 if test "$1" == "upload_stm32"
 then
-    sudo singularity run --app upload_stm32 --bind arduino_project:/arduino_project arduino_utils
+    sudo singularity run --app upload_stm32 --bind /home/zoef/arduino_project:/arduino_project arduino_utils
 fi
 
-if test "$1" == "build"
-then
-   sudo singularity run --app build --bind ino_project:/ino_project arduino_utils
-fi
 #if test "$1" == "monitor"
 #then
 #   sudo singularity run --app monitor arduino_utils
 #fi
 
-sudo service zoef_ros start
+sudo service zoef_ros start || /bin/true
