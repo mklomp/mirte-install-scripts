b0213af9cc0d1c247fbb8367d353703c5a525bc9
Martin Klomp <m.klomp@tudelft.nl>
2020-04-01 13:59:02 +0200

    Fix for blinking script

--- network_setup.sh
+++ network_setup.sh
@@ -1,5 +1,7 @@
 #bin/bash
 
+ZOEF_SRC_DIR=/usr/local/src/zoef
+
 if [ ! -f /etc/ssid ]; then
     UNIQUE_ID=$(openssl rand -hex 3)
     ZOEF_SSID=Zoef_$(echo ${UNIQUE_ID^^})
@@ -16,14 +18,12 @@ sudo service dnsmasq stop
 sleep 15
 
 iwgetid -r
-
 if [ $? -eq 0 ]; then
     printf 'Skipping WiFi Connect\n'
-    sudo /home/zoef/zoef_install_scripts/blink.sh $(hostname -I)
+    sudo $ZOEF_SRC_DIR/zoef_install_scripts/blink.sh $(hostname -I)
 else
     printf 'Starting WiFi Connect\n'
-    sudo /home/zoef/zoef_install_scripts/blink.sh $UNIQUE_ID > /home/zoef/blink.log &
+    sudo $ZOEF_SRC_DIR/zoef_install_scripts/blink.sh $UNIQUE_ID > /home/zoef/blink.log &
     sudo wifi-connect -o 8080 -p `cat /etc/wifi_pwd` -s `cat /etc/ssid`
     sudo service dnsmasq start
 fi
-
