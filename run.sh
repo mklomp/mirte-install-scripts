13de7c126ef2e12b7d662c4fe60f2f9f133ec339
Martin Klomp <m.klomp@tudelft.nl>
2020-04-06 14:40:36 +0000

    Fixes for STM32 upload from OPi

--- run.sh
+++ run.sh
@@ -5,15 +5,15 @@ sudo service zoef_ros stop || /bin/true
 echo 
 if test -z $1  || test "$1" == "upload"
 then
-    sudo singularity run --app upload --bind /home/zoef/arduino_project:/arduino_project arduino_utils
+    sudo singularity run --app upload --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
 fi
 if test "$1" == "upload_old"
 then
-    sudo singularity run --app upload_old --bind /home/zoef/arduino_project:/arduino_project arduino_utils
+    sudo singularity run --app upload_old --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
 fi
 if test "$1" == "upload_stm32"
 then
-    sudo singularity run --app upload_stm32 --bind /home/zoef/arduino_project:/arduino_project arduino_utils
+    sudo singularity run --app upload_stm32 --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
 fi
 
 #if test "$1" == "monitor"
