144b781d293d7c87e0889abbd76ec79b41b2d3e9
Martin Klomp <m.klomp@tudelft.nl>
2020-04-06 14:32:54 +0000

    Fixed link to firmata

--- install_firmata_project.sh
+++ install_firmata_project.sh
@@ -9,5 +9,6 @@ cd /home/zoef/arduino_project/FirmataPlusDue
 wget https://github.com/MrYsLab/pymata-aio/raw/master/FirmataPlus/libraries.zip
 unzip libraries.zip
 rm -rf libraries.zip
+ln -s $ZOEF_SRC_DIR/zoef_firmata libraries/FirmataPlusDue
 ln -s $ZOEF_SRC_DIR/zoef_firmata/libraries/OpticalEncoder libraries/
 ln -s $ZOEF_SRC_DIR/zoef_firmata/examples/Firmata*/Firmata*.ino ./FirmataPlusDue.ino
