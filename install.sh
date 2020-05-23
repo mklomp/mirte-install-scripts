5d89075b7bbeb7ab16147b2a9aba69352e3ffae5
Martin Klomp <m.klomp@tudelft.nl>
2020-05-23 20:23:03 +0200

    Seperately building and uploading

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@
 sudo apt install -y singularity-container
 sudo rm -rf arduino_utils
 sudo singularity build --sandbox arduino_utils Singularity
+sudo ./run.sh build
+sudo ./run.sh build_old
+sudo ./run.sh build_stm32
 
 # Add zoef to dialout
 sudo adduser zoef dialout
--- run.sh
+++ run.sh
@@ -3,13 +3,33 @@
 sudo service zoef_ros stop || /bin/true
 
 echo 
-if test -z $1  || test "$1" == "upload"
+if test -z $1
+then
+    sudo singularity run --app build --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
+    sudo singularity run --app upload --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
+fi
+
+if test "$1" == "build"
+then
+    sudo singularity run --app build --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
+fi
+if test "$1" == "upload"
 then
     sudo singularity run --app upload --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
+fi
+
+if test "$1" == "build_old"
+then
+    sudo singularity run --app build_old --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
 fi
 if test "$1" == "upload_old"
 then
     sudo singularity run --app upload_old --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
+fi
+
+if test "$1" == "build_stm32"
+then
+    sudo singularity run --app upload_stm32 --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
 fi
 if test "$1" == "upload_stm32"
 then
