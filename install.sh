4d7cdb6309ed0b582627bf45aa103aa7ddd7b76c
Martin Klomp <m.klomp@tudelft.nl>
2020-08-18 14:56:13 +0000

    Added possibility to upload own arduino ino code

--- install.sh
+++ install.sh
@@ -3,15 +3,19 @@
 ZOEF_SRC_DIR=/usr/local/src/zoef
 
 # Install our own arduino libraries
-ln -s /usr/local/src/zoef/zoef-arduino-libraries/OpticalEncoder /home/zoef/arduino_project/FirmataExpress/libraries
+ln -s $ZOEF_SRC_DIR/zoef-arduino-libraries/OpticalEncoder /home/zoef/arduino_project/FirmataExpress/libraries
+
+# Install Blink example code
+mkdir /home/zoef/arduino_project/Blink
+ln -s $ZOEF_SRC_DIR/zoef_arduino/Blink.ino /home/zoef/arduino_project/Blink
 
 # Install singularity image
 sudo apt install -y singularity-container
 sudo rm -rf arduino_utils
 sudo singularity build --sandbox arduino_utils Singularity
-sudo ./run.sh build
-sudo ./run.sh build_old
-sudo ./run.sh build_stm32
+sudo ./run.sh build FirmataExpress
+sudo ./run.sh build_nano
+sudo ./run.sh build_nano_old
 
 # Add zoef to dialout
 sudo adduser zoef dialout
--- run.sh
+++ run.sh
@@ -2,38 +2,13 @@
 
 sudo service zoef_ros stop || /bin/true
 
-echo 
+echo
 if test -z $1
 then
-    sudo singularity run --app build --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
-    sudo singularity run --app upload --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
-fi
-
-if test "$1" == "build"
-then
-    sudo singularity run --app build --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
-fi
-if test "$1" == "upload"
-then
-    sudo singularity run --app upload --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
-fi
-
-if test "$1" == "build_old"
-then
-    sudo singularity run --app build_old --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
-fi
-if test "$1" == "upload_old"
-then
-    sudo singularity run --app upload_old --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
-fi
-
-if test "$1" == "build_stm32"
-then
-    sudo singularity run --app build_stm32 --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils
-fi
-if test "$1" == "upload_stm32"
-then
-    sudo singularity run --app upload_stm32 --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef /usr/local/src/zoef/zoef_arduino/arduino_utils
+    sudo singularity run --app build --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils FirmataExpress
+    sudo singularity run --app upload --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils FirmataExpress
+else
+    sudo singularity run --app $1 --bind /home/zoef/arduino_project:/arduino_project --bind /usr/local/src/zoef:/usr/local/src/zoef arduino_utils $2
 fi
 
 #if test "$1" == "monitor"
