e4639cf718221bb58e6c4459119637c9369fdfa5
Martin Klomp <m.klomp@tudelft.nl>
2020-08-24 19:28:22 +0000

    Added pin numbers to blink

--- Blink.ino
+++ Blink.ino
@@ -1,5 +1,22 @@
-// Pin PB_12 should have an LED connected to it.
-int led = PB_12;
+// Zoef by default has the following pin connections:
+//
+// B3, A15  - PWM pins for motor A
+// B14, B15 - PWM pins for motor B
+//          - analog input intensity sensor left
+//          - analog input intensity sensor right
+//          - ping for ultrasonic sensor front
+//          - echo for ultrasonic sensor front
+//          - ping for ultrasnoic sensor side
+//          - echo for ultrasonic sensor side
+
+// Zoef does not use LEDs by default. 
+//
+// PB12     - left LED
+// PB13     - right LED
+
+
+// This is a standard blink setup
+int led = PB12;
 
 void setup() {                
   pinMode(led, OUTPUT);     
