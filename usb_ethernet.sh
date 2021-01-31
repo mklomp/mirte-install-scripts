5f85dd9ba624b89aecd5d5ab61b29012ba65e86d
Martin Klomp <m.klomp@tudelft.nl>
2021-01-31 09:40:33 +0100

    Changed usb ip addresses

--- usb_ethernet.sh
+++ usb_ethernet.sh
@@ -17,7 +17,7 @@ sudo $ZOEF_SRC_DIR/zoef_install_scripts/ev3-usb.sh up `ls /sys/class/udc | tail 
 
 #sudo ip address add 192.168.42.3/24 dev usb0
 #sudo ifconfig usb0 up
-sudo ip address add 192.168.42.3/24 dev usb1
+sudo ip address add 192.168.43.1/24 dev usb1
 sudo ifconfig usb1 up
 
 #TODO: make persitent
@@ -33,14 +33,14 @@ sudo iptables -A FORWARD -i wlan0 -o usb1 -m state --state RELATED,ESTABLISHED -
 sudo iptables -A FORWARD -i usb1 -o wlan0 -j ACCEPT
 
 #and forward all traffic to locahost
-sudo iptables -t nat -A POSTROUTING -i usb1 -d 192.168.42.3 -j DNAT --to-destination 127.0.0.1
+sudo iptables -t nat -A POSTROUTING -i usb1 -d 192.168.43.1 -j DNAT --to-destination 127.0.0.1
 
 
 
 # For now we have to start the dhcp server before wificonnect. Not needed 
 # after we moved to different namespaces
 # For some reason we neet to set the dns-server manually
-sudo dnsmasq --address=/#/192.168.42.3 --dhcp-range=192.168.42.10,192.168.42.100 --conf-file --domain-needed --bogus-priv --server=8.8.8.8 --dhcp-option=option:dns-server,8.8.8.8 --interface=usb1 --except-interface=lo --bind-interfaces
+sudo dnsmasq --address=/#/192.168.43.1 --dhcp-range=192.168.43.10,192.168.43.100 --conf-file --domain-needed --bogus-priv --server=8.8.8.8 --dhcp-option=option:dns-server,8.8.8.8 --interface=usb1 --except-interface=lo --bind-interfaces
 
 
 
