95c6a54fcf3608e6725d40c514767b9fa3c74f15
Martin Klomp <m.klomp@tudelft.nl>
2021-02-19 14:47:07 +0000

    Added verbose and fixed ROS stop/start for nano

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+#TODO: script should have format ./run.sh [build|upload] mcu_type arduino_folder
+# with mcu_type and arduino_folder optional
+
 # Stop ROS when uploading new code
-if test "$1" == "upload"
+if [[ $1 == "upload"* ]]
 then
     sudo service zoef_ros stop || /bin/true
 fi
@@ -9,34 +12,34 @@ fi
 # Different build scripts
 if test "$1" == "build"
 then
-   arduino-cli compile --fqbn STM32:stm32:GenF1:pnum=BLACKPILL_F103C8,upload_method=dfu2Method,xserial=generic,usb=CDCgen,xusb=FS,opt=osstd,rtlib=nano /home/zoef/arduino_project/$2
+   arduino-cli -v compile --fqbn STM32:stm32:GenF1:pnum=BLACKPILL_F103C8,upload_method=dfu2Method,xserial=generic,usb=CDCgen,xusb=FS,opt=osstd,rtlib=nano /home/zoef/arduino_project/$2
 fi
 if test "$1" == "build_nano"
 then
-   arduino-cli compile --fqbn arduino:avr:nano:cpu=atmega328 /home/zoef/arduino_project/$2
+   arduino-cli -v compile --fqbn arduino:avr:nano:cpu=atmega328 /home/zoef/arduino_project/$2
 fi
 if test "$1" == "build_nano_old"
 then
-   arduino-cli compile --fqbn arduino:avr:nano:cpu=atmega328old /home/zoef/arduino_project/$2
+   arduino-cli -v compile --fqbn arduino:avr:nano:cpu=atmega328old /home/zoef/arduino_project/$2
 fi
 
 # Different upload scripts
 if test "$1" == "upload"
 then
-   sudo arduino-cli upload -p /dev/ttyACM0 --fqbn STM32:stm32:GenF1:pnum=BLACKPILL_F103C8,upload_method=dfu2Method,xserial=generic,usb=CDCgen,xusb=FS,opt=osstd,rtlib=nano /home/zoef/arduino_project/$2
+   sudo arduino-cli -v upload -p /dev/ttyACM0 --fqbn STM32:stm32:GenF1:pnum=BLACKPILL_F103C8,upload_method=dfu2Method,xserial=generic,usb=CDCgen,xusb=FS,opt=osstd,rtlib=nano /home/zoef/arduino_project/$2
 fi
 if test "$1" == "upload_nano"
 then
-   sudo arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano:cpu=atmega328 /home/zoef/arduino_project/$2
+   sudo arduino-cli -v upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano:cpu=atmega328 /home/zoef/arduino_project/$2
 fi
 if test "$1" == "upload_nano_old"
 then
-   sudo arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano:cpu=atmega328old /home/zoef/arduino_project/$2
+   sudo arduino-cli -v upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano:cpu=atmega328old /home/zoef/arduino_project/$2
 fi
 
 
 # Start ROS again
-if test "$1" == "upload"
+if [[ $1 == "upload"* ]]
 then
     if test "$2" == "Telemetrix4Arduino"
     then
