fd91db7f2162a5ce188a651a05942378a0ebcffc
Martin Klomp <m.klomp@tudelft.nl>
2022-03-02 11:14:14 +0100

    Using nginx as reverse proxy to server everytign on port 80

--- install_web.sh
+++ install_web.sh
@@ -29,26 +29,9 @@ npm install .
 #npm -g install wetty
 deactivate_node
 
-# Foward for 80 to 3000
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-sudo apt install -y iptables-persistent
-#sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-port 3000
-#sudo bash -c "iptables-save > /etc/iptables/rules.v4"
-
-# TODO: iptables(-save) do now work from chroot, so saving it manually
-sudo bash -c 'echo "*filter" > /etc/iptables/rules.v4'
-sudo bash -c 'echo ":INPUT ACCEPT [1042:276951]" >> /etc/iptables/rules.v4'
-sudo bash -c 'echo ":FORWARD ACCEPT [0:0]" >> /etc/iptables/rules.v4'
-sudo bash -c 'echo ":OUTPUT ACCEPT [1039:277247]" >> /etc/iptables/rules.v4'
-sudo bash -c 'echo "COMMIT" >> /etc/iptables/rules.v4'
-sudo bash -c 'echo "*nat" >> /etc/iptables/rules.v4'
-sudo bash -c 'echo ":PREROUTING ACCEPT [1:169]" >> /etc/iptables/rules.v4'
-sudo bash -c 'echo ":INPUT ACCEPT [1:169]" >> /etc/iptables/rules.v4'
-sudo bash -c 'echo ":OUTPUT ACCEPT [3:905]" >> /etc/iptables/rules.v4'
-sudo bash -c 'echo ":POSTROUTING ACCEPT [3:905]" >> /etc/iptables/rules.v4'
-sudo bash -c 'echo "-A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 3000" >> /etc/iptables/rules.v4'
-sudo bash -c 'echo "COMMIT" >> /etc/iptables/rules.v4'
+# Install nginx (as reverse proxy to all services)
+sudo apt install -y nginx
+sudo cp $MIRTE_SRC_DIR/mirte-install-scripts/nginx.conf /etc/nginx/
 
 # Add systemd service
 sudo rm /lib/systemd/system/mirte-web-interface.service
