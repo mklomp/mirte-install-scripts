49f8d755de12d9d647ce5c064855119bab5a0c31
Martin Klomp <m.klomp@tudelft.nl>
2022-03-02 21:51:00 +0100

    Fixed dependecy for docs

--- install_mirte.sh
+++ install_mirte.sh
@@ -63,9 +63,10 @@ pip3 install numpy
 
 # Install Mirte documentation
 cd $MIRTE_SRC_DIR/mirte-documentation
-sudo apt install python3.8-venv
+sudo apt install -y python3.8-venv libenchant-dev
 python3 -m venv docs-env
 source docs-env/bin/activate
+pip install docutils==0.16.0
 pip install sphinx sphinx-prompt sphinx-tabs sphinx-rtd-theme sphinxcontrib-spelling sphinxcontrib-napoleon
 mkdir -p _modules/catkin_ws/src
 cd _modules
