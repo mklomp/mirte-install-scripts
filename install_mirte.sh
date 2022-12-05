594e41d094c6d720916117560a7bc9480720d2d8
mklomp <m.klomp@tudelft.nl>
2022-12-05 09:38:50 +0100

    Fixed pip dependencies

--- install_mirte.sh
+++ install_mirte.sh
@@ -71,8 +71,8 @@ cd $MIRTE_SRC_DIR/mirte-documentation
 sudo apt install -y python3.8-venv libenchant-dev
 python3 -m venv docs-env
 source docs-env/bin/activate
-pip install docutils==0.16.0
-pip install wheel sphinx sphinx-prompt sphinx-tabs sphinx-rtd-theme sphinxcontrib-spelling sphinxcontrib-napoleon
+pip install docutils==0.16.0 sphinx-tabs==3.2.0  #TODO: use files to freeze versions
+pip install wheel sphinx sphinx-prompt sphinx-rtd-theme sphinxcontrib-spelling sphinxcontrib-napoleon
 mkdir -p _modules/catkin_ws/src
 cd _modules
 ln -s $MIRTE_SRC_DIR/mirte-python .
