cdcbb933e7f9406f83b8aad09818ba519616580b
mklomp <m.klomp@tudelft.nl>
2023-10-23 13:16:04 +0200

    Fixing jupyter installation

--- install_jupyter_ros.sh
+++ install_jupyter_ros.sh
@@ -7,7 +7,8 @@ sudo apt install -y python3 python3-venv python3-dev git libffi-dev
 
 # create and activate virtualenv
 # Due to a build error on numpy we need to install numpy and
-# padnas globally and us it in the virtual environment
+# pandas globally and us it in the virtual environment
+# TODO: check if (micro/mino)mamba/conda could fix this
 cd /home/mirte || exit
 sudo apt install -y python3-numpy python3-pandas
 python3 -m venv jupyter --system-site-packages
@@ -15,9 +16,13 @@ source /home/mirte/jupyter/bin/activate
 
 # install jupyros
 pip3 install wheel
-pip3 install markupsafe==2.0.1
-pip3 install jupyter bqplot==0.12.18 pyyaml ipywidgets
-pip3 install jupyros
+pip3 install markupsafe==2.0.1 pyzmq==24 zipp==3.1.0
+pip3 install ipython==8.7.0 ipykernel==6.17.1 ipywidgets==7.7.2 \
+             jupyter-client==7.4.8 jupyter-core==5.1.0 \
+             nbclient==0.7.2 nbconvert==7.2.6 nbformat==5.7.0 \
+             qtconsole==5.4.0 traitlets==5.6.0
+pip3 install notebook==6.5.2 bqplot==0.12.18 pyyaml
+pip3 install --pre jupyros==0.7.0a0
 jupyter nbextension enable --py --sys-prefix jupyros
 deactivate
 sudo chown -R mirte:mirte /home/mirte/jupyter
